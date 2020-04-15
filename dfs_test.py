a0558879305cae2d7fc739c76cfbaef7d9dfc524
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2020-04-15 20:12:31 +0300

    Update dfs_test.py

--- dfs_test.py
+++ dfs_test.py
@@ -1,74 +1,7 @@
 class TestSimpleGraph(unittest.TestCase):
+	
 	def setUp(self):
 		self.simpleGraph = SimpleGraph(16)
-
-	def testDFS6Vertex(self):
-		for i in range(6):
-			self.simpleGraph.AddVertex(i)
-		self.simpleGraph.AddEdge(1,0)
-		self.simpleGraph.AddEdge(1,2)
-		self.simpleGraph.AddEdge(1,3)
-		self.simpleGraph.AddEdge(2,5)
-		self.simpleGraph.AddEdge(3,4)
-		x = self.simpleGraph.DepthFirstSearch(2,4)
-		y = []
-		for i in range(len(x)):
-			y.append(x[i].Value)
-		assert y == [2,1,3,4]
-
-	def testDFS15Vertex(self):
-		for i in range(15):
-			self.simpleGraph.AddVertex(i)
-		self.simpleGraph.AddEdge(0,1)
-		self.simpleGraph.AddEdge(0,2)
-		self.simpleGraph.AddEdge(2,1)
-		self.simpleGraph.AddEdge(3,1)
-		self.simpleGraph.AddEdge(3,2)
-		self.simpleGraph.AddEdge(4,1)
-		self.simpleGraph.AddEdge(5,2)
-		self.simpleGraph.AddEdge(5,6)
-		self.simpleGraph.AddEdge(4,6)
-		self.simpleGraph.AddEdge(5,8)
-		self.simpleGraph.AddEdge(4,7)
-		self.simpleGraph.AddEdge(7,8)
-		self.simpleGraph.AddEdge(7,9)
-		self.simpleGraph.AddEdge(8,10)
-		self.simpleGraph.AddEdge(9,11)
-		self.simpleGraph.AddEdge(11,13)
-		self.simpleGraph.AddEdge(14,13)
-		self.simpleGraph.AddEdge(14,12)
-		self.simpleGraph.AddEdge(12,10)
-		x = self.simpleGraph.DepthFirstSearch(0,14)
-		y = []
-		for i in range(len(x)):
-			y.append(x[i].Value)
-		assert y == [0,1,2,5,6,4,7,8,10,12,14]
-
-	def testDFSNoWay(self):
-		for i in range(15):
-			self.simpleGraph.AddVertex(i)
-		self.simpleGraph.AddEdge(0,1)
-		self.simpleGraph.AddEdge(0,2)
-		self.simpleGraph.AddEdge(2,1)
-		self.simpleGraph.AddEdge(3,1)
-		self.simpleGraph.AddEdge(3,2)
-		self.simpleGraph.AddEdge(4,1)
-		self.simpleGraph.AddEdge(5,2)
-		self.simpleGraph.AddEdge(5,6)
-		self.simpleGraph.AddEdge(4,6)
-		self.simpleGraph.AddEdge(5,8)
-		self.simpleGraph.AddEdge(4,7)
-		self.simpleGraph.AddEdge(7,8)
-		self.simpleGraph.AddEdge(7,9)
-		self.simpleGraph.AddEdge(8,10)
-		self.simpleGraph.AddEdge(9,11)
-		self.simpleGraph.AddEdge(11,13)
-		self.simpleGraph.AddEdge(12,10)
-		x = self.simpleGraph.DepthFirstSearch(0,14)
-		y = []
-		for i in range(len(x)):
-			y.append(x[i].Value)
-		assert y == []
 
 	def testDFSDtoA(self):
 		letters = ["A","B","C","D","E"]
@@ -77,14 +10,17 @@ class TestSimpleGraph(unittest.TestCase):
 		self.simpleGraph.AddEdge(0,1)
 		self.simpleGraph.AddEdge(0,2)
 		self.simpleGraph.AddEdge(0,3)
+		self.simpleGraph.AddEdge(1,0)
 		self.simpleGraph.AddEdge(1,3)
 		self.simpleGraph.AddEdge(1,4)
+		self.simpleGraph.AddEdge(2,0)
 		self.simpleGraph.AddEdge(2,3)
+		self.simpleGraph.AddEdge(3,1)
+		self.simpleGraph.AddEdge(3,2)
 		self.simpleGraph.AddEdge(3,4)
+		self.simpleGraph.AddEdge(4,1)
+		self.simpleGraph.AddEdge(4,3)
 		x = self.simpleGraph.DepthFirstSearch(3,0)
 		assert x[0].Value == "D"
 		assert x[1].Value == "A"
 		assert len(x) == 2
-		
-if __name__ == '__main__':
-	unittest.main()
