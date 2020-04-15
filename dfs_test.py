599be0a56c4b112fb62474e38460710d280cd141
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2020-04-15 19:52:38 +0300

    Update dfs_test.py

--- dfs_test.py
+++ dfs_test.py
@@ -70,5 +70,21 @@ class TestSimpleGraph(unittest.TestCase):
 			y.append(x[i].Value)
 		assert y == []
 
+	def testDFSDtoA(self):
+		letters = ["A","B","C","D","E"]
+		for i in letters:
+			self.simpleGraph.AddVertex(i)
+		self.simpleGraph.AddEdge(0,1)
+		self.simpleGraph.AddEdge(0,2)
+		self.simpleGraph.AddEdge(0,3)
+		self.simpleGraph.AddEdge(1,3)
+		self.simpleGraph.AddEdge(1,4)
+		self.simpleGraph.AddEdge(2,3)
+		self.simpleGraph.AddEdge(3,4)
+		x = self.simpleGraph.DepthFirstSearch(3,0)
+		assert x[0].Value == "D"
+		assert x[1].Value == "A"
+		assert len(x) == 2
+		
 if __name__ == '__main__':
 	unittest.main()
